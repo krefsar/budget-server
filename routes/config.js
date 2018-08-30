ea8598f9d86f19b8420bbbed7108328587a8fee4
Evan Martinez <evanm.design@gmail.com>
2018-08-30 19:46:52 -0400

    set up nested resource routes

--- routes/config.js
+++ routes/config.js
@@ -1,19 +1,19 @@
 module.exports = function() {
   this.get('/users/:id');
-  // this.patch('/users/:id');
+  this.patch('/users/:id');
 
   this.get('/budgets');
   this.get('/budgets/:id');
-  // this.patch('/budgets/:id');
-  // this.delete('/budgets/:id');
-  // this.post('/budgets');
+  this.patch('/budgets/:id');
+  this.delete('/budgets/:id');
+  this.post('/budgets');
 
   this.get('/transactions');
-  // this.post('/transactions');
+  this.post('/transactions');
 
   this.get('/expenses');
   this.get('/expenses/:id');
-  // this.patch('/expenses/:id');
-  // this.delete('/expenses/:id');
-  // this.post('/expenses');
+  this.patch('/expenses/:id');
+  this.delete('/expenses/:id');
+  this.post('/expenses');
 }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(cors());
 
 app.use('/api', require('./routes'));
 
