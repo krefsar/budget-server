09d28bfb1a0e9870f7e7ea2c17e9e1bf0de90675
Evan Martinez <evanm.design@gmail.com>
2018-09-02 23:28:19 -0400

    some more routes filled out

--- routes/deleteSingle.js
+++ routes/deleteSingle.js
@@ -1,1 +1,14 @@
-module.exports = (req, res) => {}
+const pluralize = require('pluralize');
+
+module.exports = (req, res) => {
+  const { modelType, id } = req.params;
+
+  const model = require(`../models/${pluralize.singular(modelType)}`);
+
+  model
+    .forge({ id })
+    .destroy()
+    .then(() => {
+      res.status(204).send({});
+    });
+}
--- routes/getSingle.js
+++ routes/getSingle.js
@@ -1,1 +1,23 @@
-module.exports = (req, res) => {}
+const pluralize = require('pluralize');
+const Mapper = require('jsonapi-mapper');
+const mapper = new Mapper.Bookshelf();
+
+module.exports = (req, res) => {
+  const { modelType, id } = req.params;
+  const { include } = req.query;
+
+  const model = require(`../models/${pluralize.singular(modelType)}`);
+
+  const fetchOptions = {}
+
+  if (include) {
+    fetchOptions.withRelated = include.split(',');
+  }
+
+  model
+    .forge({ id })
+    .fetch(fetchOptions)
+    .then(fetchedModel => {
+      res.status(200).send(mapper.map(fetchedModel, modelType, { enableLinks: false }));
+    });
+}
